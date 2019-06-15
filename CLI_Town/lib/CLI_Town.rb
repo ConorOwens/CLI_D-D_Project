ef1750a57b1cb7e568c57af444bb0342a31ffaee
Conor Owens <conorj.owens@gmail.com>
2019-06-15 13:54:28 +0000

    Built out spell display within CLI

--- CLI_Town/lib/CLI_Town.rb
+++ CLI_Town/lib/CLI_Town.rb
@@ -29,8 +29,9 @@ class CommandLineInterface
     puts "Say 'all spells' to see a complete list of castable spells." 
     puts "Say 'spells by letter' to see a list of spells starting with a specific letter. "
     puts "Say 'spells by class' to see a list of spells according to what class can cast them."
+    puts "At any point, say the name of a spell to see more specifics about that spell."
     input = gets.strip
-    if input == "all spells"
+    if input.downcase == "all spells"
       counter = 1
       puts "A dark and heavy grimoire approaches and reveals its secrets."
       CLI_Town::Spell.all.each do |spell|
@@ -38,31 +39,62 @@ class CommandLineInterface
         counter += 1 
       end
       input = gets.strip
-    elsif input == "spells by letter"
+    elsif input.downcase == "spells by letter"
       puts "A voice whispers down the eternal hallways. 'Which letter would you like to see more closely?'"
       input_alpha = gets.strip
       puts "A scroll unfurls in front of you with spells listed upon it."
       counter = 1
       Spell.all.each do |spell|
-        if spell.alpha[-2] == input_alpha
+        if spell.alpha[-2] == input_alpha.upcase
           puts "#{counter}. #{spell.name}"
           counter += 1 
         end
       end
       input = gets.strip
-    elsif input == "spells by class"
-      puts "Many voices sing out at once. It's hard to understand their commands but as they repeat you can discern them. 'Sorceror? Wizard? Cleric? Druid? Paladin? Bard? Ranger? CHOOSE!"
+    elsif input.downcase == "spells by class"
+      puts "Many voices sing out at once. It's hard to understand their commands but as they repeat you can discern them. 'Sorceror? Wizard? Cleric? Druid? Paladin? Bard? Ranger? CHOOSE!'"
       input_beta = gets.strip
       puts "The voices quiet, and an ancient and cracked leather book floats slowly towards you with pages opened."
       counter = 1
       Spell.all.each do |spell|
-        if spell.classes.include?(input_beta)
+        if spell.classes.include?(input_beta.capitalize)
           puts "#{counter}. #{spell}"
           counter += 1 
         end
       end
       input = gets.strip
-    elsif Spell.all.name.include?(input)
+    elsif Spell.all.name.include?(input.capitalize)
+      puts "A single shining parchment darts from between the books. The spell you seek is emblazoned in filligree on it's surface."
+      Spell.all.each do |spell|
+        if Spell.all.name == input.capitalize
+          #attr_accessor :name, :classes, :level, :school, :subschool, :descriptor, :components, :cast_time, :range, :effect, :duration, :saving_throw, :SR, :description, :alpha, :url
+          puts "Name: #{spell.name}"
+          counter = 0 
+          while counter < spell.classes.size 
+            puts "#{spell.classes[counter]} [#{spell.level[counter]}]"
+            counter += 1
+          end
+          puts "Class(es): #{spell.classes}"
+          if spell.school
+            puts "School: #{spell.school}"
+          end
+          if spell.subschool
+            puts "Subschool: #{spell.subschool}"
+          end
+          if spell.descriptor
+            puts "Descriptor: #{spell.descriptor}"
+          end
+          puts "Components: #{spell.components}"
+          puts "Casting Time: #{spell.cast_time}"
+          puts "Range: #{spell.range}"
+          puts "Effect: #{spell.effect}"
+          puts "Duration: #{spell.duration}"
+          puts "Saving Throw: #{spell.saving_throw}"
+          puts "SR: #{spell.sr}"
+          puts "________________________"
+          puts "#{spell.description}"
+        end
+      end
       
     elsif
   end
